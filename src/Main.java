3b2035add0ea73a2a2e34308c72768d70062973d
padmakaw <padmaka.wijayagoonawardena@trycake.com>
2017-03-02 10:23:28 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * @author : Padmaka Wijayagoonawardena
+ * @date : 3/2/17
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Person person = new Person();
+        person.setName("peter");
+        person.setEmail("peter@gmail.com");
+        System.out.println(main.savePerson(person));
+    }
+
+    public Person savePerson(Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException();
+        } else {
+            person.setId("p100");
+            return person;
+        }
+    }
+}
